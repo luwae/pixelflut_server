a75d3b04279c03bd46d49d0b9cb6de3ad23abf65
luwae <l.waeldele@t-online.de>
2024-02-17 22:34:50 +0100

    revert screen size

--- param.h
+++ param.h
@@ -4,7 +4,7 @@
 #define CONN_BUF_SIZE 1024
 #define TEX_SIZE_X 512
 #define TEX_SIZE_Y 512
-#define SCREEN_SIZE_X 1024
-#define SCREEN_SIZE_Y 1024
+#define SCREEN_SIZE_X 512
+#define SCREEN_SIZE_Y 512
 
 #endif
